5c06c50908314e9f1ae5874fc915f7ac44fef664
Max Shydlovskiy <shidlymax@gmail.com>
2023-01-16 00:41:59 +0200

    45-52_pages

---
+++ irb_output.rb
@@ -0,0 +1,30 @@
+puts "First line\nSecond line\nThird line"
+# First line
+# Second line
+# Third line
+#  => nil
+puts "\tIndented line"
+#         Indented line
+#  => nil
+puts "\"It's okay,\" he said."
+# "It's okay," he said.
+#  => nil
+puts "One backslash: \\"
+# One backslash: \
+#  => nil
+puts '\n\t\"'
+# \n\t\"
+# => nil
+puts 42.class
+# Integer
+#  => nil
+puts "hello".class
+# String
+#  => nil
+puts true.class
+# TrueClass
+#  => nil
+puts rand(100)   # 1-100, but without 100
+# 23
+#  => nil
+rand(100) + 1  # 1-100, include 100
