2971082916cd9f93feee2916b173d23e79d2058c
Max Shydlovskiy <shidlymax@gmail.com>
2023-01-16 00:22:45 +0200

    1-45_pages

---
+++ exercise_page_39-40.rb
@@ -0,0 +1,30 @@
+42 / 6
+# => 7
+name = "Zaphod"
+# => "Zaphod"
+name.upcase
+# => "ZAPHOD"
+"Zaphod".upcase
+# => "ZAPHOD"
+name.reverse
+# => "dophaZ"
+name.upcase.reverse
+# => "DOHPAZ"
+name.class
+# => String
+name * 3
+# => "ZaphodZaphodZaphod"
+5 > 4
+# => true
+number = -32
+# => -32
+number.abs
+# => 32
+-32.abs
+# => 32
+number += 10
+# => -22
+rand(25)
+# => 11
+number.class
+# => Integer
